a589a0f965beb31b5ca7d595d6545ebb9cfa572f
katsho <katsho@katsho.com>
2022-08-01 13:51:13 +0100

    adding sub dict to binary search

--- MatchList/python/iteration3.py
+++ MatchList/python/iteration3.py
@@ -20,7 +20,19 @@ Key b not in B
 >>> main({"a":1, "b":"2", "d":{"e":"e"}},{"a":1, "b":"2", "c":"three"})
 Key d not in B
 
+>>> main({"a":1, "b":"2", "d":{"e":"e"}},{"a":1, "b":"2", "c":"three", "d":{"e":"e"}})
+All keys in A found in B
 
+>>> main({"a":1, "b":"2", "d":{"e":"e", "f":"f"}},{"a":1, "b":"2", "c":"three", "d":{"e":"e"}})
+Key f not in B
+Key d not in B
+
+>>> main({"a":1, "b":"2", "d":{"e":"e", "f":"f"}},{"a":1, "b":"2", "c":"three", "d":{"e":"e","f":1}})
+Key f not in B
+Key d not in B
+
+>>> main({"a":1, "b":"2", "d":{"e":"e", "f":"f"}},{"a":1, "b":"2", "c":"three", "d":{"e":"e","f":"f"}})
+All keys in A found in B
 """
 from math import floor
 import sys
@@ -28,12 +40,15 @@ from xmlrpc.client import Boolean
 
 #need to return found, is less than center, key is greater than center, key no match (length = 1)
 def findKey(item, b) -> Boolean:
+ 
     dictLength = len(b)
     dictCenter = floor(dictLength/2)
     dictCenterKeyName = list(b.keys())[dictCenter]
     itemKey = item[0]
     itemValue = item[1]
     if itemKey == dictCenterKeyName and (type(itemValue)==type(b[dictCenterKeyName])):
+        if (type(itemValue) is dict): 
+            return findKeys(itemValue,b[dictCenterKeyName])
         return True  #found
     if itemKey != dictCenterKeyName and dictLength==1:
         return False #not found
@@ -49,6 +64,8 @@ def findKeys(a,b) -> Boolean:
         if not result:
             print ("Key " + item[0] + " not in B")
             return False
+        
+
     
     return True
 
